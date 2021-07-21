be03c0c10b7c594dadff4bd853eea111f13ddbc5
over40dev <ted@over40.dev>
2021-07-21 18:28:24 -0230

    Array Cardio Day 1 (complete)

--- Array Cardio Day 1/script.js
+++ Array Cardio Day 1/script.js
@@ -23,26 +23,87 @@ const people = [
 ];
 
 // Array.prototype.filter()
-    // 1. Filter the list of inventors for those who were born in the 1500's
+// 1. Filter the list of inventors for those who were born in the 1500's
+const inv50 = inventors.filter(i=>i.year>=1500&&i.year<1600);
+console.table(inv50);
 
-    // Array.prototype.map()
-    // 2. Give us an array of the inventors first and last names
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+const invNames = inventors.map(i => [i.first, i.last]);
+console.table(invNames);
+const invNamesString = inventors.map(i => `${i.first} ${i.last}`);
+console.log(invNamesString);
 
-    // Array.prototype.sort()
-    // 3. Sort the inventors by birthdate, oldest to youngest
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+const invByAge = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
+console.table(invByAge);
 
-    // Array.prototype.reduce()
-    // 4. How many years did all the inventors live all together?
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+const invAllLived = inventors.reduce((sum, i) => {
+  return sum + (i.passed - i.year)
+},0);
+console.log(invAllLived);
 
-    // 5. Sort the inventors by years lived
+// 5. Sort the inventors by years lived
+const invYearsLived = inventors
+.map(i=>[i.first,i.last,i.passed-i.year])
+.sort((a,b) => a[2] > b[2] ? 1 : -1);
+console.table(invYearsLived);
 
-    // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-    // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// code in console when on this web page:
+/*
+code: arrStreets.map(s => s.text).filter(s => s.includes('de'));
 
+result:
+Array(12) [ "Boulevard de l'Amiral-Bruix", "Boulevard des Capucines", "Boulevard de la Chapelle", "Boulevard de Clichy", "Boulevard de l'Hôpital", "Boulevard des Italiens", "Boulevard de la Madeleine", "Boulevard de Magenta", "Boulevard Marguerite-de-Rochechouart", "Boulevard de Sébastopol", … ]
+​
+0: "Boulevard de l'Amiral-Bruix"
+​
+1: "Boulevard des Capucines"
+​
+2: "Boulevard de la Chapelle"
+​
+3: "Boulevard de Clichy"
+​
+4: "Boulevard de l'Hôpital"
+​
+5: "Boulevard des Italiens"
+​
+6: "Boulevard de la Madeleine"
+​
+7: "Boulevard de Magenta"
+​
+8: "Boulevard Marguerite-de-Rochechouart"
+​
+9: "Boulevard de Sébastopol"
+​
+10: "Boulevard de Strasbourg"
+​
+11: "Boulevard de la Zone"
+​
+length: 12
+*/
 
-    // 7. sort Exercise
-    // Sort the people alphabetically by last name
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const peopleLastName = people.sort((a,b) => a > b ? 1 : -1);
+console.log(peopleLastName);
 
-    // 8. Reduce Exercise
-    // Sum up the instances of each of these
-    const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'jet' ];
+
+const transportCount = data.reduce((sum, d) => {
+  
+  if (!sum[d]) {
+    sum[d] = 1;
+  } else {
+    sum[d]++;
+  }
+  return sum;
+}, {});
+console.log(transportCount);
